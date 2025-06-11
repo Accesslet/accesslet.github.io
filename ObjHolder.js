ad4018fccd6dabe865139b7d660588aa0ff07944
Accesslet <75988100+Accesslet@users.noreply.github.com>
2025-06-11 21:01:24 +0500

    Add files via upload

---
+++ ObjHolder.js
@@ -0,0 +1,13 @@
+var objectHolder = {};
+
+objectHolder.embedObject = function(url, cont, width, height){
+    var ruffle = window.RufflePlayer.newest(),
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
+        });
+
+    player.load({ url: url });
+}
+
